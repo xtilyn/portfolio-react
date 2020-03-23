a2b997316382be77ee6c09b1668af066aec874fd
Christilyn Arjona <christilynarjona@outlook.com>
2020-03-22 21:34:12 -0600

    🏗 setup mobile & desktop view for home pg

---
+++ src/pages/HomeView/MobileView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MobileView extends Component {
+    render() {
+        return (
+            <div>
+                asd
+            </div>
+        )
+    }
+}
