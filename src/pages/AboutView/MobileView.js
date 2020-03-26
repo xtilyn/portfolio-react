591bc4f3ad5143d71a83e2c0c1bf3e0f8d6e300c
Christilyn Arjona <christilynarjona@outlook.com>
2020-03-25 20:49:44 -0600

    setup about view mobile

--- src/pages/AboutView/MobileView.js
+++ src/pages/AboutView/MobileView.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import HeaderMobile from "../../HeaderMobile";
+import { Column } from "simple-flexbox";
+import { colorPrimary } from "../../constants/app_colors";
+import { SwipeableDrawer } from "@material-ui/core";
+import NavDrawer from "../../components/NavDrawer";
 
-export default function MobileView() {
-  return <div>about mobile view</div>;
+const rootStyle = {
+  background: colorPrimary,
+  width: "100vw",
+  height: "100vh"
+};
+
+export default function MobileView(props) {
+  const [isDrawerOpen, toggleDrawer] = useState(false);
+
+  const toggleDrawerTrueFalse = () => {
+    toggleDrawer(!isDrawerOpen);
+  };
+
+  return (
+    <div style={rootStyle}>
+      <React.Fragment key="left">
+        <HeaderMobile toggleDrawer={toggleDrawerTrueFalse} />
+        <SwipeableDrawer
+          anchor="left"
+          open={isDrawerOpen}
+          onClose={toggleDrawerTrueFalse}
+          onOpen={toggleDrawerTrueFalse}
+          style={{ background: colorPrimary }}
+        >
+          <NavDrawer selectedItem={2} />
+        </SwipeableDrawer>
+        about view mobile
+      </React.Fragment>
+    </div>
+  );
 }
