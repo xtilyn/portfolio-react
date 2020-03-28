92a4cca2e281113ab930965f899939202eb2f6df
christilynarjona <christilynarjona@outlook.com>
2020-03-28 14:04:49 -0600

    Updates

---
+++ precache-manifest.70462fc207fba7fe015e2ae3f5f23e32.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f9b063b443d888ff6bbaf6830e147f",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "9885e77c674aad10c128",
+    "url": "/portfolio-react/static/css/2.49d78008.chunk.css"
+  },
+  {
+    "revision": "bf952057bb31ad3e983c",
+    "url": "/portfolio-react/static/css/main.236cb45c.chunk.css"
+  },
+  {
+    "revision": "9885e77c674aad10c128",
+    "url": "/portfolio-react/static/js/2.4455c5a4.chunk.js"
+  },
+  {
+    "revision": "f2e468b2a9e5e062245a55e7492c93dc",
+    "url": "/portfolio-react/static/js/2.4455c5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf952057bb31ad3e983c",
+    "url": "/portfolio-react/static/js/main.231dfbab.chunk.js"
+  },
+  {
+    "revision": "38cc46e1fd5c05c02767",
+    "url": "/portfolio-react/static/js/runtime-main.6f440d9c.js"
+  },
+  {
+    "revision": "f065119a72d8cf921656b43a1b38a788",
+    "url": "/portfolio-react/static/media/Cuiseek_group.f065119a.png"
+  },
+  {
+    "revision": "f9d59d0d0e022d74c9f30a991ac23d3e",
+    "url": "/portfolio-react/static/media/DevsSocial_group.f9d59d0d.png"
+  },
+  {
+    "revision": "0c78cc3fd809f8608698b9f1feaf5bab",
+    "url": "/portfolio-react/static/media/Jumpbox_group.0c78cc3f.png"
+  },
+  {
+    "revision": "6b4e0c78bfe3c6a257eb643cec87a709",
+    "url": "/portfolio-react/static/media/Localodge_group.6b4e0c78.png"
+  },
+  {
+    "revision": "9e63d92e994da2a1d67e9ec3d87cd7ae",
+    "url": "/portfolio-react/static/media/Mafia_group.9e63d92e.png"
+  },
+  {
+    "revision": "9b5f720ac6c9f366de7acfe4fac3adba",
+    "url": "/portfolio-react/static/media/Medpass_group.9b5f720a.png"
+  },
+  {
+    "revision": "0d828c1685b0a8a521badea452510b84",
+    "url": "/portfolio-react/static/media/Neuwly_group.0d828c16.png"
+  },
+  {
+    "revision": "367ea6826647d582d3668624c3f1d9cc",
+    "url": "/portfolio-react/static/media/Teampics_group.367ea682.png"
+  },
+  {
+    "revision": "bb37778a40cc7ac245cd3115985616d6",
+    "url": "/portfolio-react/static/media/logo.bb37778a.png"
+  },
+  {
+    "revision": "191cc9f50f3b76b9617cb383f19acb7d",
+    "url": "/portfolio-react/static/media/montserrat-latin-100.191cc9f5.woff2"
+  },
+  {
+    "revision": "370318464551d5f25b0f0a78f374faac",
+    "url": "/portfolio-react/static/media/montserrat-latin-100.37031846.woff"
+  },
+  {
+    "revision": "bdeaeb79db315697bd173a55b097dc18",
+    "url": "/portfolio-react/static/media/montserrat-latin-100italic.bdeaeb79.woff2"
+  },
+  {
+    "revision": "ecf7d49386e8f265878e735db34a7c4b",
+    "url": "/portfolio-react/static/media/montserrat-latin-100italic.ecf7d493.woff"
+  },
+  {
+    "revision": "1fc98e126a3d152549240e6244d7e669",
+    "url": "/portfolio-react/static/media/montserrat-latin-200.1fc98e12.woff"
+  },
+  {
+    "revision": "85d5ef9db7f2dc6979172a4a3b2c57cb",
+    "url": "/portfolio-react/static/media/montserrat-latin-200.85d5ef9d.woff2"
+  },
+  {
+    "revision": "49095760a498d024fe1a85a078850df9",
+    "url": "/portfolio-react/static/media/montserrat-latin-200italic.49095760.woff2"
+  },
+  {
+    "revision": "fe46cf8b9462c820457d3bf537e4057f",
+    "url": "/portfolio-react/static/media/montserrat-latin-200italic.fe46cf8b.woff"
+  },
+  {
+    "revision": "7c3daf12b706645b5d3710f863a4da04",
+    "url": "/portfolio-react/static/media/montserrat-latin-300.7c3daf12.woff2"
+  },
+  {
+    "revision": "8dc95fab9cf98d02ca8d76e97d3dff60",
+    "url": "/portfolio-react/static/media/montserrat-latin-300.8dc95fab.woff"
+  },
+  {
+    "revision": "3fe16939288856e8e828fa2661bf2354",
+    "url": "/portfolio-react/static/media/montserrat-latin-300italic.3fe16939.woff"
+  },
+  {
+    "revision": "f20b178ca2024a5eac8e42e6649db86c",
+    "url": "/portfolio-react/static/media/montserrat-latin-300italic.f20b178c.woff2"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/portfolio-react/static/media/montserrat-latin-400.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/portfolio-react/static/media/montserrat-latin-400.bc3aa95d.woff2"
+  },
+  {
+    "revision": "5cad650422a7184467af5a4d17b264c4",
+    "url": "/portfolio-react/static/media/montserrat-latin-400italic.5cad6504.woff2"
+  },
+  {
+    "revision": "d191f22af3bb50902b99ac577f81a322",
+    "url": "/portfolio-react/static/media/montserrat-latin-400italic.d191f22a.woff"
+  },
+  {
+    "revision": "8b763220218ffc11c57c84ddb80e7b26",
+    "url": "/portfolio-react/static/media/montserrat-latin-500.8b763220.woff"
+  },
+  {
+    "revision": "92d16e458625f4d2c8940f6bdca0ff09",
+    "url": "/portfolio-react/static/media/montserrat-latin-500.92d16e45.woff2"
+  },
+  {
+    "revision": "47bfcca6b69d6a9acca7a8bff17193e2",
+    "url": "/portfolio-react/static/media/montserrat-latin-500italic.47bfcca6.woff2"
+  },
+  {
+    "revision": "72c01f753c3940c0b9cb6bf2389caddf",
+    "url": "/portfolio-react/static/media/montserrat-latin-500italic.72c01f75.woff"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/portfolio-react/static/media/montserrat-latin-600.6fb1b562.woff2"
+  },
+  {
+    "revision": "7c839d15a6f54e7025ba8c0c4b333e8f",
+    "url": "/portfolio-react/static/media/montserrat-latin-600.7c839d15.woff"
+  },
+  {
+    "revision": "60789af1c9338ed1a9546722ec54b4f7",
+    "url": "/portfolio-react/static/media/montserrat-latin-600italic.60789af1.woff2"
+  },
+  {
+    "revision": "f3d4de8d0afb19e777c79032ce828e3d",
+    "url": "/portfolio-react/static/media/montserrat-latin-600italic.f3d4de8d.woff"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/portfolio-react/static/media/montserrat-latin-700.39d93cf6.woff2"
+  },
+  {
+    "revision": "80f10bd382f0df1cd650fec59f3c9394",
+    "url": "/portfolio-react/static/media/montserrat-latin-700.80f10bd3.woff"
+  },
+  {
+    "revision": "8c98142b425630821139c24bd1698700",
+    "url": "/portfolio-react/static/media/montserrat-latin-700italic.8c98142b.woff"
+  },
+  {
+    "revision": "ba136d97b14e82284dd595e257f11c47",
+    "url": "/portfolio-react/static/media/montserrat-latin-700italic.ba136d97.woff2"
+  },
+  {
+    "revision": "9a9befcf50d64f9d2d19d8b1d1984add",
+    "url": "/portfolio-react/static/media/montserrat-latin-800.9a9befcf.woff"
+  },
+  {
+    "revision": "b7018be9ed6cd94da8b6675b3a468c3b",
+    "url": "/portfolio-react/static/media/montserrat-latin-800.b7018be9.woff2"
+  },
+  {
+    "revision": "540ffdd223d1a9ad3d4e678e1a23372e",
+    "url": "/portfolio-react/static/media/montserrat-latin-800italic.540ffdd2.woff2"
+  },
+  {
+    "revision": "897086f99f4e1f45e6b1e9368527d0bc",
+    "url": "/portfolio-react/static/media/montserrat-latin-800italic.897086f9.woff"
+  },
+  {
+    "revision": "26d42c9428780e545a540bbb50c84bce",
+    "url": "/portfolio-react/static/media/montserrat-latin-900.26d42c94.woff"
+  },
+  {
+    "revision": "58cd789700850375b834e8b6776002eb",
+    "url": "/portfolio-react/static/media/montserrat-latin-900.58cd7897.woff2"
+  },
+  {
+    "revision": "451157bc8861fe54f523b3669a3def71",
+    "url": "/portfolio-react/static/media/montserrat-latin-900italic.451157bc.woff2"
+  },
+  {
+    "revision": "a8ec4957e1c24f5793305763ad9845b3",
+    "url": "/portfolio-react/static/media/montserrat-latin-900italic.a8ec4957.woff"
+  },
+  {
+    "revision": "6296e2836a5f98bc92f98d2dc4156bd2",
+    "url": "/portfolio-react/static/media/phone_android.6296e283.png"
+  },
+  {
+    "revision": "fdf56c48a482b71683490bb97ea06f3c",
+    "url": "/portfolio-react/static/media/xd_icon.fdf56c48.png"
+  }
+]);
