872d6039d3bd0976aa8c7066aa4f7cb24977adf0
Christilyn Arjona <christilynarjona@outlook.com>
2020-03-29 23:01:45 -0600

    🐛 reset selected item index AppPreview

--- src/pages/PortfolioView/index.js
+++ src/pages/PortfolioView/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import { useMediaQuery } from "react-responsive";
 import MobileView from "./MobileView";
 import DesktopView from "./DesktopView";
+import { selectPortfolioItem } from "../../redux/actions/actions-types";
+import { connect } from "react-redux";
 
-const PortfolioView = props => {
+const PortfolioViewConnected = props => {
   const isDesktop = useMediaQuery({ minWidth: 992 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
   const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  props.selectPortfolioItem(0);
 
   return (
     <div>
@@ -16,5 +20,15 @@ const PortfolioView = props => {
     </div>
   );
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  selectPortfolioItem: appPreviewSelectedItem =>
+      dispatch(selectPortfolioItem(appPreviewSelectedItem))
+});
+
+const PortfolioView = connect(
+  null,
+  mapDispatchToProps
+)(PortfolioViewConnected);
 
 export default PortfolioView;
